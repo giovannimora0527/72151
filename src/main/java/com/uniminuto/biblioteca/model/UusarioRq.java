c29891483074aac143f7ed478af625a27a0ba10d
ewdev <edison.wica@gmail.com>
2025-04-03 19:28:13 -0500

    update

---
+++ src/main/java/com/uniminuto/biblioteca/model/UusarioRq.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uniminuto.biblioteca.model;
+
+/**
+ *
+ * @author ewdev
+ */
+@Data
+public class UusarioRq {
+    private messag
+}
